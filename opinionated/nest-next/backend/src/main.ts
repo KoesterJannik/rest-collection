1ffe6fa853f054d93009cd851a906f365ec96c0d
Jannik Köster <koesterjannik1998@gmail.com>
2023-05-12 11:40:35 +0200

    added swagger

--- opinionated/nest-next/backend/src/main.ts
+++ opinionated/nest-next/backend/src/main.ts
@@ -3,6 +3,8 @@ import { AppModule } from './app.module';
 import { PrismaService } from './services/prisma.service';
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { DocumentBuilder } from '@nestjs/swagger';
+import { SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -10,6 +12,15 @@ async function bootstrap() {
   const prismaService = app.get(PrismaService);
   prismaService.enableShutdownHooks(app);
   const configService = app.get(ConfigService);
+  const config = new DocumentBuilder()
+    .setTitle('Cats example')
+    .setDescription('The cats API description')
+    .setVersion('1.0')
+    .addTag('cats')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   const PORT = Number(configService.get('PORT'));
   await app.listen(PORT);
 }
