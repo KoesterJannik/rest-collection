66f2fcc558f4ba4d5a8213248c2ee0a39ddd4d52
Jannik Köster <koesterjannik1998@gmail.com>
2023-05-12 10:38:15 +0200

    init

--- opinionated/nest-next/backend/src/main.ts
+++ opinionated/nest-next/backend/src/main.ts
@@ -7,6 +7,6 @@ async function bootstrap() {
   const prismaService = app.get(PrismaService);
   prismaService.enableShutdownHooks(app);
   const PORT = process.env.PORT || 3000;
-  await app.listen(3000);
+  await app.listen(PORT);
 }
 bootstrap();
